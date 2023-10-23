effadd1a6aea97677f6f9537e55a4074f9e9e074
mortadhamansour <mortamansour@gmail.com>
2023-10-23 13:01:52 +0100

    web fondamentals

---
+++ Programming Basics/Week 1/Day 2/morta.js
@@ -0,0 +1,10 @@
+function add(num1, num2){
+        console.log("Summing Numbers!");//Summing Numbers!
+        console.log("num1 is: " + num1);//num1 is: 10
+        console.log("num2 is: " + num2);//num1 is: 20
+        var sum = num1 + num2;//sum = 10+20
+        console.log(sum);//30 
+    }
+    
+    
+    
---
+++ Programming Basics/Week 1/Day 3/Practice/group.js
@@ -0,0 +1,21 @@
+var j = 0    
+for(var year = 1996; year <= 2023; year++){
+    
+if(year == 2023){
+console.log("HAPPY 27 years of JAVASCRIPT! 🎈🎈🎈" )
+}
+else if(j==10){
+console.log("HAPPY DACADE JS")
+j = 0
+}
+
+
+
+
+
+else{
+console.log(year + "JS is awesome")
+}
+j = j+1
+
+}
