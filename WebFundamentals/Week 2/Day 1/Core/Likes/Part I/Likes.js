428be9b1227d98aaa9714ae9e0d1696eea67dd5e
mortadhamansour <mortamansour@gmail.com>
2023-10-31 16:52:27 +0100

    webfundamentals

---
+++ WebFundamentals/Week 2/Day 1/Core/Likes/Part I/Likes.js
@@ -0,0 +1,3 @@
+function count(){
+    document.querySelector("#like").innerHTML++;
+}
---
+++ WebFundamentals/Week 2/Day 1/Core/Likes/Part II/Likes II.js
@@ -0,0 +1,9 @@
+function count1(){
+    document.querySelector('#like1').innerHTML++
+}
+function count2(){
+    document.querySelector('#like2').innerHTML++
+}
+function count3(){
+    document.querySelector('#like3').innerHTML++
+}
